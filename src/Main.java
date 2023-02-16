5a1b84e000dab4c8efd53873c90fa939eb6ceabe
Тарас <Acroyd1984@gmail.com>
2023-02-17 00:53:22 +0300

    Home work variable 2 intermediate

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+        task2();
+        task3();
+    }
+        public static void task1() {
         System.out.println("Задача 1");
+        byte a = 1;
+        System.out.println("Значение переменной а с типом byte равно " + a);
+        short b = 30000;
+        System.out.println("Значение переменной b с типом short равно " + b);
+        int c = 2000000000;
+        System.out.println("Значение переменной c с типом int равно " + c);
+        long d = 3L;
+        System.out.println("Значение переменной d с типом long равно " + d);
+        float e = 1.2f;
+        System.out.println("Значение переменной e с типом byte равно " + e);
+        double g = 3.123;
+        System.out.println("Значение переменной g с типом double равно " + g);
+    }
+        public static void task2() {
+        System.out.println("Задача 2");
+        float a = 27.12f;
+        long b = 987678965549L;
+        double c = 2.786;
+        short d = 569;
+        short e = -159;
+        int f = 27897;
+        byte g = 67;
+    }
+        public static void task3() {
+        System.out.println("Задача 3");
 
     }
 }
